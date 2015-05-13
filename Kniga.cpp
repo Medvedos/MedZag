d1767cfdd474b10c18e937ecee76853232307d80
Medvedev Zagmilin <kawaii10001@gmail.com>
2015-05-13 07:04:45 -0400

    Подготовка сохранения

---
+++ Kniga.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "Kniga.h"
+/**
+@file	Kniga.cpp
+@brief	Заголовочный файл класса Персоны
+@copyright	Иу5-27М
+@author	Бобров, Зажмилин, Медведев
+@date	13-05-2015
+@version	alpha
+\par Использует классы
+	@ref Kniga.h
+\par Содержит класс
+	@ref Kniga
+*/
+
+Kniga::Kniga(void)
+{
+}
+
+
+Kniga::~Kniga(void)
+{
+}
---
+++ Posetitel.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "Posetitel.h"
+/**
+@file	Posetitel.cpp
+@brief	Заголовочный файл класса Персоны
+@copyright	Иу5-27М
+@author	Бобров, Зажмилин, Медведев
+@date	13-05-2015
+@version	alpha
+\par Использует классы
+	@ref Posetitel.h
+\par Содержит класс
+	@ref Posetitel
+*/
+
+Posetitel::Posetitel(void)
+{
+}
+
+
+Posetitel::~Posetitel(void)
+{
+}
